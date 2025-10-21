1c6f73dcfd563348e40e605c035f23fc31ccc469
Avijit255 <avijit252005@gmail.com>
2025-10-22 00:37:27 +0530

    routes create

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,25 @@
-
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+
+  const router = useRouter();
+
   return (
     <View
-    className="bg-blue-600"
+      className="bg-blue-600"
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen Abhijit dey 255.</Text>
+      <Text className="text-3xl">abhijit is a cow.</Text>
+      <TouchableOpacity onPress={()=> router.push("/home")}>
+        <Text>
+          Changin route
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
