ef2f9d3b8ea9b611f393625a5f520b4484e44f4b
Avijit255 <avijit252005@gmail.com>
2025-10-22 21:42:00 +0530

    / page done

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Colors } from '../../assets/Colors'
-import { Ionicons } from '@expo/vector-icons'
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+import { Colors } from "../../assets/Colors";
 
 const TabLayout = () => {
   return (
-  <Tabs screenOptions={{
-    headerShown:false,
-      tabActiveTintColor: Colors.PRIMARY,
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabActiveTintColor: Colors.PRIMARY,
         tabInActiveTintColor: Colors.dark.text,
         tabBarStyle: {
           backgroundColor: Colors.SECONDARY,
@@ -19,17 +18,37 @@ const TabLayout = () => {
           fontSize: 15,
           fontWeight: "bold",
         },
-      
-  }} >
-    <Tabs.Screen name='home' options={{title:"Home" , tabBarIcon:({color})=>(
-      <Ionicons name='home'size={24} color={color} />
-    ) 
-    }} />
-    <Tabs.Screen name='history' options={{title:"History"}} />
-    <Tabs.Screen name='profile' options={{title:"Profile"}} />
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="time" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person-sharp" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 
-  </Tabs>
-  )
-}
-
-export default TabLayout
+export default TabLayout;
